43e474e3676939cab06cd3c8a79a42f69abfb6c0
Ian D. Scott <ian@perebruin.com>
2015-01-17 15:38:07 -0800

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='communitytags',
+      version='0.1',
+      description='Community provided tags for packages.',
+      author='Ian D. Scott',
+      author_email='ian@perebruin.com',
+      license = "MIT",
+      url='http://github.com/ids1024/archcommunitytags-tools/',
+      scripts = ['communitytags'],
+     )
