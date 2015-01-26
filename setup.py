e8aea8d1410d0a83cace1e644d39c031eb15285f
Ian D. Scott <ian@perebruin.com>
2015-01-26 09:46:43 -0800

    Install man page

--- setup.py
+++ setup.py
@@ -9,5 +9,8 @@ setup(name='communitytags',
       author_email='ian@perebruin.com',
       license = "GPL3",
       url='http://github.com/ids1024/archcommunitytags-tools/',
+      data_files=[
+          ('/usr/share/man/man1', ['communitytags.1']),
+          ],
       scripts = ['communitytags'],
      )
